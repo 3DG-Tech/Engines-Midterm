30f985d8fd38c846634bc59d91d89a09f09ca4e0
Dhruva <dhruva.guruprasad@gmail.com>
2020-10-09 17:35:45 -0400

    checkpoints and deathbox triggers are working

---
+++ Engines Midterm - 100700271/Assets/ChangeCheckpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCheckpoint : MonoBehaviour
+{
+    public GameObject previousCheckpoint;
+
+    public GameObject Player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Destroy(previousCheckpoint);
+           
+        }
+    }
+}
